ce8f54204f902fa010e9fd2407ca66f9e764bd6d
Sjpeti97 <sjpeti97@gmail.com>
2022-02-15 14:39:28 +0100

    refactor: delete front-end

--- progress-tracker/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import App from './App';
-test('renders learn react link', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/GET IT DONE/i);
-    expect(linkElement).toBeInTheDocument();
-    });
